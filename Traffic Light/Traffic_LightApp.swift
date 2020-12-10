fa0b281eec2be3d162534ceee3020eeba0060990
Yaroslav <yaroslav.olex@gmail.com>
2020-12-10 15:53:48 +0200

    Initial Commit

---
+++ Traffic Light/Traffic_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_LightApp.swift
+//  Traffic Light
+//
+//  Created by Yaroslav on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
